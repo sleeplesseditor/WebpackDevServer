d4cd346a9ea1ea3a1c97c5544ce54806288bf734
Josh Bryson <josh@bryson.me.uk>
2020-05-18 22:36:50 +0100

    Nodemon and Babel package updates implemented

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -13,6 +15,10 @@ module.exports = {
   devServer: {
     contentBase: 'dist',
     overlay: true,
+    hot: true,
+    stats: {
+      colors: true
+    }
   },
   module: {
     rules: [
@@ -39,15 +45,6 @@ module.exports = {
       {
         test: /\.html$/,
         use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].html'
-            }
-          },
-          {
-            loader: 'extract-loader',
-          },
           {
             loader: 'html-loader',
             options: {
@@ -77,4 +74,10 @@ module.exports = {
       }
     ],
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HTMLWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
 };
---
+++ src/server/express.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import path from 'path';
+
+const server = express();
+const webpack = require('webpack');
+const config = require('../../config/webpack.dev');
+const compiler = webpack(config)
+
+const webpackDevMiddleware = require('webpack-dev-middleware')(
+  compiler,
+  config.devServer
+)
+
+const webpackHotMiddleware = require('webpack-hot-middleware')(compiler)
+
+server.use(webpackDevMiddleware)
+server.use(webpackHotMiddleware)
+console.log('Middleware enabled')
+
+const staticMiddleware = express.static('dist')
+server.use(staticMiddleware);
+
+const port = 5000;
+server.listen(port, () => {
+  console.log(`Server is Listening on http://localhost:${port}`)
+});
