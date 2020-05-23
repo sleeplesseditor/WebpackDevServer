a06a2f6e78222715b95df14c92050bf92ffd5836
Josh Bryson <josh@bryson.me.uk>
2020-05-23 20:30:25 +0100

    React variant implemented with hot loading

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 require('@babel/runtime/regenerator')
+require('react-hot-loader/patch')
+require('@babel/register')
 require('webpack-hot-middleware/client?reload=true')
 require('./main.css')
 require('./images/link.jpg')
 require('./index.html')
+require('./app')
