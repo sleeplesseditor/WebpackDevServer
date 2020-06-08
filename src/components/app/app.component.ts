de0b34b10ecea7e89d7780d955b22a9b88ee386d
Josh Bryson <josh@bryson.me.uk>
2020-06-08 21:27:50 +0100

    Input component added

--- src/components/app/app.component.ts
+++ src/components/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component} from '@angular/core';
 
 @Component({
   selector: 'root-app',
-  styles: [],
-  template: require("./app.component.html")
+  styles: [require('./app.component.css').toString()],
+  template: require('./app.component.html')
 })
 
 export class AppRoot {
